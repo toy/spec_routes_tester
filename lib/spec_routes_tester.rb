56954f73ef75d7b5d769059b0aa58c17b0f8388b
Boba Fat <ivan@workisfun.ru>
2008-09-03 19:59:23 +0400

    added simple resource helper works only with first level routes

--- lib/spec_routes_tester.rb
+++ lib/spec_routes_tester.rb
@@ -8,21 +8,42 @@ class SpecRoutesTester
   end
 
   def method_missing(method, *args, &block)
-    args.length.should == 2
+    puts args.map(&:inspect).join(', ')
+    if args.length == 2 && args[0].is_a?(String) && args[1].is_a?(Hash)
+      route = args[0][0, 1] == '/' ? args[0] : "/#{args[0]}"
+      params = args[1]
+      http_method = (params.delete(:conditions) || {})[:method]
+      essential_params = params.reject{ |k, v| [:action, :controller].include?(k) }
 
-    route = args[0][0, 1] == '/' ? args[0] : "/#{args[0]}"
-    params = args[1]
-    http_method = (params.delete(:conditions) || {})[:method]
-    essential_params = params.reject{ |k, v| [:action, :controller].include?(k) }
+      @example.route_for(params).should == route
+      @example.params_from(http_method, route).should == params
 
-    @example.route_for(params).should == route
-    @example.params_from(http_method, route).should == params
+      begin
+        @example.process params[:action], essential_params
+      rescue ActiveRecord::RecordNotFound
+      end
 
-    begin
-      @example.process params[:action], essential_params
-    rescue ActiveRecord::RecordNotFound
+      @example.send("#{method}_path", essential_params).should == route
+    else
+      super(method, *args, &block)
     end
+  end
 
-    @example.send("#{method}_path", essential_params).should == route
+  def resources(*entities)
+    entities.each do |entity|
+      plural = entity.to_s
+      singular = plural.singularize
+      with_options :controller => plural do |m|
+        m.send       "#{plural}", "#{plural}",          :action => "index",   :conditions => {:method => :get}
+        m.send       "#{plural}", "#{plural}",          :action => "create",  :conditions => {:method => :post}
+        m.send "new_#{singular}", "#{plural}/new",      :action => "new",     :conditions => {:method => :get}
+        m.with_options :id => '1' do |mi|
+          mi.send "edit_#{singular}", "#{plural}/1/edit", :action => "edit",    :conditions => {:method => :get}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "show",    :conditions => {:method => :get}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "update",  :conditions => {:method => :put}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "destroy", :conditions => {:method => :delete}
+        end
+      end
+    end
   end
 end
