c131c959404f4136579675ffd40bbf46dad620f7
Boba Fat <ivan@workisfun.ru>
2008-09-03 20:01:10 +0400

    forgot about puts

--- lib/spec_routes_tester.rb
+++ lib/spec_routes_tester.rb
@@ -7,8 +7,25 @@ class SpecRoutesTester
     @example = example
   end
 
+  def resources(*entities)
+    entities.each do |entity|
+      plural = entity.to_s
+      singular = plural.singularize
+      with_options :controller => plural do |m|
+        m.send       "#{plural}", "#{plural}",          :action => "index",   :conditions => {:method => :get}
+        m.send       "#{plural}", "#{plural}",          :action => "create",  :conditions => {:method => :post}
+        m.send "new_#{singular}", "#{plural}/new",      :action => "new",     :conditions => {:method => :get}
+        m.with_options :id => '1' do |mi|
+          mi.send "edit_#{singular}", "#{plural}/1/edit", :action => "edit",    :conditions => {:method => :get}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "show",    :conditions => {:method => :get}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "update",  :conditions => {:method => :put}
+          mi.send      "#{singular}", "#{plural}/1",      :action => "destroy", :conditions => {:method => :delete}
+        end
+      end
+    end
+  end
+
   def method_missing(method, *args, &block)
-    puts args.map(&:inspect).join(', ')
     if args.length == 2 && args[0].is_a?(String) && args[1].is_a?(Hash)
       route = args[0][0, 1] == '/' ? args[0] : "/#{args[0]}"
       params = args[1]
@@ -26,24 +43,6 @@ class SpecRoutesTester
       @example.send("#{method}_path", essential_params).should == route
     else
       super(method, *args, &block)
-    end
-  end
-
-  def resources(*entities)
-    entities.each do |entity|
-      plural = entity.to_s
-      singular = plural.singularize
-      with_options :controller => plural do |m|
-        m.send       "#{plural}", "#{plural}",          :action => "index",   :conditions => {:method => :get}
-        m.send       "#{plural}", "#{plural}",          :action => "create",  :conditions => {:method => :post}
-        m.send "new_#{singular}", "#{plural}/new",      :action => "new",     :conditions => {:method => :get}
-        m.with_options :id => '1' do |mi|
-          mi.send "edit_#{singular}", "#{plural}/1/edit", :action => "edit",    :conditions => {:method => :get}
-          mi.send      "#{singular}", "#{plural}/1",      :action => "show",    :conditions => {:method => :get}
-          mi.send      "#{singular}", "#{plural}/1",      :action => "update",  :conditions => {:method => :put}
-          mi.send      "#{singular}", "#{plural}/1",      :action => "destroy", :conditions => {:method => :delete}
-        end
-      end
     end
   end
 end
